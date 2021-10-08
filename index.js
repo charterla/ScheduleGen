966aa750cccb2bc126c24fd3d1dfbeb5555351cf
charterla <2330911429eric2@gmail.com>
2021-10-09 00:12:31 +0800

    Alpha_0.2.1

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ var now_page = 0
 
 function changePage(page_num){
     console.log(now_page, page_num);
-    if(page_num == now_page)return;
+    if(page_num == now_page)return false;
     else {
         console.log(document.getElementById("button_"+String(now_page)).classList, document.getElementById("button_"+String(page_num)).classList)
         document.getElementById("button_"+String(now_page)).classList.remove("button_now_page");
+        document.getElementById("page_"+String(now_page)).style.display = "none";
+
         document.getElementById("button_"+String(page_num)).classList.add("button_now_page");
+        document.getElementById("page_"+String(page_num)).style.display = "block";
 
         now_page = page_num;
+        return true;
     }
+}
+
+function inputClass(){
+    if(!changePage(0))return;
+}
+
+function generation(){
+    if(!changePage(1))return;
 }
